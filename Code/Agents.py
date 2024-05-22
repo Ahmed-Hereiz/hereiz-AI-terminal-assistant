a16505edccc92aec62cd2b88d1e79c5d010f278a
Ahmed Hereiz <ahmed.hany.fathi@gmail.com>
2024-05-23 00:05:12 +0300

    make classes for each agent inhereit form Agent

--- Code/Agents.py
+++ Code/Agents.py
@@ -1,30 +1,61 @@
-from langchain_google_genai import ChatGoogleGenerativeAI, HarmBlockThreshold, HarmCategory
+from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain.prompts.prompt import PromptTemplate
 from langchain.chains import ConversationChain
+import time
 
 class Agent:
     def __init__(self, api_key, agent_template, model, temperature, safety_settings):
-        
-        self.agent_llm = ChatGoogleGenerativeAI(google_api_key=api_key,
-                                            model=model,
-                                            temperature=temperature,
-                                            safety_settings=safety_settings
-                                            )
-        
+        self.initialize_agent(api_key, agent_template, model, temperature, safety_settings)
+
+    def initialize_agent(self, api_key, agent_template, model, temperature, safety_settings):
+        self.agent_llm = ChatGoogleGenerativeAI(
+            google_api_key=api_key,
+            model=model,
+            temperature=temperature,
+            safety_settings=safety_settings
+        )
         self.prompt_template = PromptTemplate(input_variables=["input"], template=agent_template)
 
-
+class Planner(Agent):
     def make_plan(self, user_input):
-
-        conversation = ConversationChain(prompt=self.prompt_template,llm=self.agent_llm, verbose=False)
-
+        conversation = ConversationChain(prompt=self.prompt_template, llm=self.agent_llm, verbose=False)
         plan = conversation.predict(input=user_input)
-        return plan
-    
-    def make_code(self, planner_input):
+        with open("tmp_plan", "w") as file:
+            file.write(plan)
 
-        conversation = ConversationChain(prompt=self.prompt_template,llm=self.agent_llm, verbose=False)
+class Coder(Agent):
+    def write_code(self):
+        with open("tmp_plan","r") as file:
+            plan = file.read()
 
-        code = conversation.predict(input=planner_input)
-        return code
-    
+        conversation = ConversationChain(prompt=self.prompt_template, llm=self.agent_llm, verbose=False)
+        code = conversation.predict(input=plan)
+        with open("code_file.py", "w") as file:
+            file.write(code)
+
+class Debuger(Agent):
+    def debug_code(self):
+        with open("code_file.py", "r") as file:
+            code = file.read()
+
+        conversation = ConversationChain(prompt=self.prompt_template, llm=self.agent_llm, verbose=False)
+        debugged_code = conversation.predict(input=code)
+        
+        timestamp = time.strftime("%Y%m%d-%H%M%S")
+        debugged_filename = f"debugged_code_{timestamp}.py"
+
+        with open(debugged_filename, "w") as debugged_file:
+                debugged_file.write(debugged_code)
+
+        print(f"Debugged code saved as {debugged_filename}")
+
+
+class Tester(Agent):
+    def test_script(self):
+        pass
+
+
+class Documenter(Agent):
+    def document_file(self):
+        pass
+
--- Code/agents_code.py
+++ Code/agents_code.py
@@ -3,7 +3,7 @@ import json
 from colorama import Fore
 from langchain_google_genai import HarmBlockThreshold, HarmCategory
 
-from Agents import Agent
+from Agents import Agent, Planner, Coder
 
 with open('../config.json', 'r') as f:
     config = json.load(f)
@@ -19,16 +19,15 @@ safety_settings = {
     for category, threshold in config['safety_settings'].items()
 }
 
-plan_agent = Agent(config['api_key'],plan_template,config['model'],config['planner_agent_temperature'],safety_settings)
-code_agent = Agent(config['api_key'],code_template,config['model'],config['coder_agent_temperature'],safety_settings)
+plan_agent = Planner(config['api_key'],plan_template,config['model'],config['planner_agent_temperature'],safety_settings)
+code_agent = Coder(config['api_key'],code_template,config['model'],config['coder_agent_temperature'],safety_settings)
 
-user_input = "I have a images dir where inside it I have a dir named as dogs and one named as cats, I want you make full torch code to load this data from dir using dataloader then make the images shape 224,224 then make a cnn model on this data and fit the model "
+user_input = """
+write pytorch code to train a neural net on image dir named as Images, it's sub dir contains 2 dirs one named cats and one named dogs
+train a cnn on this data where the task is to make dataloaders to load from Images/ dir then make a cnn class and make the training loop
+then save the torch model.
+"""
 
-print(f"the code the user needs is : {user_input}")
-plan = plan_agent.make_plan(user_input=user_input)
-print(plan)
+plan_agent.make_plan(user_input=user_input)
 
-print("now the coder model : ")
-print("-"*100)
-code = code_agent.make_code(planner_input=plan)
-print(code)
+code_agent.write_code()
