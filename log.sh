dca8e701701edd205b22c2025145c6346370d43a
Ahmed Hereiz <ahmed.hany.fathi@gmail.com>
2024-06-05 07:30:57 +0300

    update logs.sh

--- log.sh
+++ log.sh
@@ -5,12 +5,15 @@ log_file="logs/hereiz.logs"
 log() {
     local log_level="$1"
     local log_message="$2"
-    echo "$(date +"%Y-%m-%d %H:%M:%S") [$log_level] - $log_message" >> "$log_file"
+    local script_name="$3"
+    local exit_status="$4"
+
+    echo "$(date +"%Y-%m-%d %H:%M:%S") [SCRIPT: $script_name] [$log_level] - $log_message | Exit Status: $exit_status" >> "$log_file"
 }
 
-if [ "$#" -eq 0 ]; then
-    echo "Usage: $0 [log_level] [log_message]"
+if [ "$#" -lt 3 ]; then
+    echo "Usage: $0 [log_level] [log_message] [script_name] [exit_status]"
     exit 1
 fi
 
-log "$1" "$2"
+log "$1" "$2" "$3" "$4" 
