0365ba56d970718578352e8dbfde52eadc65427f
Ahmed Hereiz <ahmed.hany.fathi@gmail.com>
2024-06-18 21:02:56 +0300

    add replace_placeholder (debuged) to replace other replace func (they will be deleted later)

--- helpers/reformat_strings.py
+++ helpers/reformat_strings.py
@@ -1,5 +1,15 @@
 import json
 
+def replace_history_sentence(template, memory_buffer):
+    return template.replace("{history}",memory_buffer)
+
+
+def replace_instructions_sentence(template, instructions):
+    return template.replace("{format_instructions}",instructions)
+
+def replace_input_sentence(template, user_input):
+    return template.replace("{input}",user_input)
+
 def replace_placeholder(template, placeholder, value):
     return template.replace(placeholder, value)
 
