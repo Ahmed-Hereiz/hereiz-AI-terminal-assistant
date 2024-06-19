28cdeac6b402f92d30ee96a78c5c29180bae95d0
Ahmed Hereiz <ahmed.hany.fathi@gmail.com>
2024-06-19 07:28:07 +0300

    add replace_placeholder to __init__.py

--- helpers/__init__.py
+++ helpers/__init__.py
@@ -2,7 +2,8 @@ from .reformat_strings import (
     replace_history_sentence,
     replace_instructions_sentence,
     replace_input_sentence,
-    extract_json_from_string
+    extract_json_from_string,
+    replace_placeholder
 )
 
 from .fetch_data import (
@@ -18,5 +19,6 @@ __all__ = [
     'extract_json_from_string',
     'extract_fetched_data',
     'concat_fetched_content',
-    'concat_links_and_titles'
+    'concat_links_and_titles',
+    'replace_placeholder'
 ]
