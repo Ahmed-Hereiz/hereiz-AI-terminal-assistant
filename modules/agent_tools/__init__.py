bd6999dbecd0709123ba489ab238f2a575962773
Ahmed Hereiz <ahmed.hany.fathi@gmail.com>
2024-06-23 04:45:20 +0300

    added readpdf and scrapegithub tools

--- modules/agent_tools/__init__.py
+++ modules/agent_tools/__init__.py
@@ -1,11 +1,16 @@
 from .base_tools import ToolKit, BaseTool
 from .scrapelink_tool import ScrapeLinkTool
 from .search_tool import SearchTool
-
+from .readpdf_tool import PDFDocReaderTool, PDFDocReaderSaverTool
+from .scrapegithub_tool import GithubAccScrapeTool, GithubAccScrapeSaveTool
 
 __all__ = [
     'ToolKit',
     'BaseTool',
     'ScrapeLinkTool',
-    'SearchTool'
+    'SearchTool',
+    'PDFDocReaderTool',
+    'PDFDocReaderSaverTool',
+    'GithubAccScrapeTool',
+    'GithubAccScrapeSaveTool'
 ]
---
+++ modules/agent_tools/readpdf_tool.py
@@ -0,0 +1,28 @@
+from typing import Any
+import PyPDF2
+from agent_tools import BaseTool
+
+
+class PDFDocReaderTool(BaseTool):
+    def execute_func(self, pdf_path) -> str:
+        
+        text = ""
+        with open(pdf_path, "rb") as file:
+            reader = PyPDF2.PdfReader(file)
+            num_pages = len(reader.pages)
+            for page_num in range(num_pages):
+                page = reader.pages[page_num]
+                text += page.extract_text()
+        return text 
+    
+
+
+class PDFDocReaderSaverTool(PDFDocReaderTool):
+    def execute_func(self, pdf_path, md_path) -> str:
+        text = super().execute_func(pdf_path)
+
+        with open(md_path, "w") as file:
+            file.write("# PDF Content \n\n")
+            file.write(text)
+
+        print(f"Content Saved to {md_path}")
