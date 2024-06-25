a9c29afc9c35d46b872fb8282d5056f684381c18
Ahmed Hereiz <ahmed.hany.fathi@gmail.com>
2024-06-25 08:50:24 +0300

    update init

--- modules/agent_prompt/__init__.py
+++ modules/agent_prompt/__init__.py
@@ -1,9 +1,11 @@
 from .base_prompt import BasePrompt
 from .simple_prompt import SimplePrompt
 from .placeholder_prompt import PlaceHoldersPrompt
+from .react_prompt import ReActPrompt
 
 __all__ = [
     'BasePrompt',
     'SimplePrompt',
-    'PlaceHoldersPrompt'
+    'PlaceHoldersPrompt',
+    'ReActPrompt'
 ]
