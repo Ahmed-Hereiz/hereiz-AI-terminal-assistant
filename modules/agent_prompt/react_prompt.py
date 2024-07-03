d67219b9aa0cbfb2d84920e7add1b52b0fefa0f9
Ahmed Hereiz <ahmed.hany.fathi@gmail.com>
2024-07-03 09:45:12 +0300

    Trying to imporve the prompt template for better ReAct

--- modules/agent_prompt/react_prompt.py
+++ modules/agent_prompt/react_prompt.py
@@ -24,13 +24,13 @@ Question: the input question you must answer
 Thought: you should always think about what to do
 Action: the action to take, should be one of [{tool_names}]
 Action Input: the input to the action Make sure to return these parameters as python list
-Observation: the result of the action (NOTE THAT in the first observation loop you don't give Observation you just output : 'waiting to use tool if available')
+Observation: the result of the action (NOTE THAT : it will be given to you, Don't write the Observation at all just use it if found to know more about the context)
 ... (this Thought/Action/Action Input/Observation can repeat N times)
 Thought: I now know the final answer
 Action: finish
 Final Answer: the final answer to the original input question
 
-YOU MUST GO THROUGH ALL OF THESE STEPS IN ORDER. DO NOT SKIP ANY STEPS.
+YOU MUST GO THROUGH ALL OF THESE STEPS IN ORDER. DO NOT SKIP ANY STEPS. AND STRICTLY FOLLOW ALL THE NOTES ABOVE.
 
 {example_workflow}
 
