a23a98708e03869f2e166b6e2dd54b6f8f5799ab
Ahmed Hereiz <ahmed.hany.fathi@gmail.com>
2024-07-13 18:31:40 +0300

    add conditional router

---
+++ modules/agent_routers/conditional_router.py
@@ -0,0 +1,16 @@
+from typing import Any
+from agent_routers import BaseRouter
+
+
+class InterruptRouter(BaseRouter):
+    def __init__(self, condition: Any, perform: Any, exec_after: float = 0):
+        super().__init__(exec_after)
+
+        self.condition = condition
+        self.perform = perform
+
+
+    def exec_router(self, check_on):
+
+        if check_on == self.condition:
+            return self.perform
