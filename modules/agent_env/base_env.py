7bc0fcb142a2eaae3e314bcac429c10f33f27800
Ahmed Hereiz <ahmed.hany.fathi@gmail.com>
2024-07-15 13:30:50 +0300

    change param from agent to env_run

--- modules/agent_env/base_env.py
+++ modules/agent_env/base_env.py
@@ -6,24 +6,24 @@ AgentType = Union[BaseRuntime, BaseRouter]
 
 
 class BaseEnv:
-    def __init__(self, agents: List[AgentType]):
+    def __init__(self, env_runs: List[AgentType]):
 
-        self.agents = agents
+        self.env_runs = env_runs
 
     def run(self):
         """modify this in every class you create that inherit from BaseEnv"""
-        for agent in self.agents:
-            print(f"agent in the agents list : {agent}")
+        for env_run in self.env_runs:
+            print(f"agent in the agents list : {env_run}")
 
         raise NotImplementedError(f"Need to implement the run method first to specify how the env will work")
         
 
     def __str__(self) -> str:
-        return f"agents in this env is {self.agents}"
+        return f"agents or routers in this env is {self.env_run}"
     
     def __add__(self, other) -> Any:
-        self.agents = self.agents + other.agents
+        self.env_runs = self.env_runs + other.env_runs
 
     def __repr__(self) -> str:
-        return f"agents in this env is {self.agents}"
+        return f"agents or routers in this env is {self.env_runs}"
     
