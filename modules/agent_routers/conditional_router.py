5a8e72a6db07c4b4d6345aaca12c1c67477909d7
Ahmed Hereiz <ahmed.hany.fathi@gmail.com>
2024-07-19 09:54:28 +0300

    add SizeConditionalRouter

--- modules/agent_routers/conditional_router.py
+++ modules/agent_routers/conditional_router.py
@@ -13,7 +13,7 @@ class ConditionalRouter(BaseRouter):
     def exec_router(self, check_on):
         """
         It's prefered to use this router as base and make other routers that inherit from it.
-        use it to make more routers that checks some conditions like type, size,outputs,etc...
+        use it to make more routers that checks some conditions like type, size, outputs, etc...
         """
 
         if check_on == self.condition:
@@ -27,3 +27,12 @@ class TypeConditionalRouter(ConditionalRouter):
 
     def exec_router(self, condition_type):
         return super().exec_router(condition_type)
+
+
+class SizeConditionalRouter(ConditionalRouter):
+    def __init__(self, condition: Any, perform: Any, exec_after: float = 0):
+        super().__init__(len(condition), perform, exec_after)
+
+
+    def exec_router(self, condition_len):
+        return super().exec_router(condition_len)
