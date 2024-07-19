55ddbcfc409cb5bbc272e061b73e90234475f825
Ahmed Hereiz <ahmed.hany.fathi@gmail.com>
2024-07-19 09:55:33 +0300

    add new routers in __init__.py

--- modules/agent_routers/__init__.py
+++ modules/agent_routers/__init__.py
@@ -1,11 +1,13 @@
 from .base_router import BaseRouter
 from .toolexec_router import ToolExecRouter
 from .interrupt_router import InterruptRouter
-from .conditional_router import ConditionalRouter
+from .conditional_router import ConditionalRouter, TypeConditionalRouter, SizeConditionalRouter
 
 __all__ = [
     'BaseRouter',
     'ToolExecRouter',
     'InterruptRouter',
-    'ConditionalRouter'
+    'ConditionalRouter',
+    'TypeConditionalRouter',
+    'SizeConditionalRouter',
 ]
