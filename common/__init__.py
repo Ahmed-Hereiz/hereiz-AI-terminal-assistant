60c28e48a1c9b124b8882f80d9b391a0fd83212c
Ahmed Hereiz <ahmed.hany.fathi@gmail.com>
2024-09-14 20:18:49 +0300

    remove safety parsing

--- common/__init__.py
+++ common/__init__.py
@@ -2,7 +2,6 @@ from .utils import (
     load_config,
     load_memory_buffer,
     load_template,
-    parse_safety_settings
 )
 from .manage_memory import MemoryManager
 
@@ -10,6 +9,5 @@ __all__ = [
     'load_config',
     'load_template',
     'load_memory_buffer',
-    'parse_safety_settings',
     'MemoryManager',
 ]
--- common/utils.py
+++ common/utils.py
@@ -1,5 +1,4 @@
 import json
-from langchain_google_genai import HarmBlockThreshold, HarmCategory
 
 def load_config(config_path):
     with open(config_path, 'r') as f:
@@ -14,10 +13,3 @@ def load_template(template_path):
 def load_memory_buffer(memory_buffer_path):
     with open(memory_buffer_path, 'r') as memory_buffer:
         return memory_buffer.read()
-
-
-def parse_safety_settings(settings):
-    return {
-        HarmCategory[category]: HarmBlockThreshold[threshold]
-        for category, threshold in settings.items()
-    }
