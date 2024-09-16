8be83bef4a2a072e9284ab116cd406ddcc7f8c61
Ahmed Hereiz <ahmed.hany.fathi@gmail.com>
2024-09-16 15:06:51 +0300

    remove useless imports

--- src/features/Search/__init__.py
+++ src/features/Search/__init__.py
@@ -1,9 +1,7 @@
 from .run_search import handle_search
-from .run_searchopen import handle_searchopen
 from .run_fullsearch import handle_fullsearch
 
 __all__ = [
     'handle_search',
-    'handle_searchopen',
     'handle_fullsearch'
 ]
