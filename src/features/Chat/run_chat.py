6a540b3a51c3dc77faecfb2ab67ba9f493a2d422
Ahmed Hereiz <ahmed.hany.fathi@gmail.com>
2024-09-29 01:32:40 +0300

    debug some  color effects

--- src/features/Chat/run_chat.py
+++ src/features/Chat/run_chat.py
@@ -2,12 +2,8 @@ from utils import get_arguments, add_root_to_path
 
 root_dir = add_root_to_path()
 from common import MemoryManager, load_config
-from modules.chat_agents import (
-    ChatLLM,ChatPrompt,ChatAgent,
-    ChatSummarizerLLM,ChatSummarizerPrompt,ChatSummarizerAgent,
-    ChatAgentsEnv
-) 
-
+from modules.chat_agents import ChatLLM, ChatPrompt, ChatAgent
+from modules.memory_agents import MemoryLLM, MemoryPrompt, MemoryAgent, MemoryReflectionEnv
 
 def hande_chat():
     config = load_config(f'{root_dir}/config/llm.json')
@@ -24,12 +20,12 @@ def hande_chat():
     chat_prompt = ChatPrompt(user_question=args.chat,memory=memory,prompt_string="")
     chat_agent = ChatAgent(llm=chat_llm,prompt=chat_prompt)
 
-    chat_summary_llm = ChatSummarizerLLM(api_key=config['api_key'],model=config['model'],temperature=0.5)
-    chat_summary_prompt = ChatSummarizerPrompt(user_question=args.chat,memory=memory)
-    chat_summary_agent = ChatSummarizerAgent(llm=chat_summary_llm,prompt=chat_summary_prompt)
+    chat_summary_llm = MemoryLLM(api_key=config['api_key'],model=config['model'],temperature=0.5)
+    chat_summary_prompt = MemoryPrompt(user_question=args.chat,memory=memory)
+    chat_summary_agent = MemoryAgent(llm=chat_summary_llm,prompt=chat_summary_prompt)
 
     
-    chat_env = ChatAgentsEnv(agents=[chat_agent,chat_summary_agent])
-    new_memory = chat_env.run()
+    chat_env = MemoryReflectionEnv(agents=[chat_agent,chat_summary_agent])
+    _, new_memory = chat_env.run()
 
     manage_memory.save_buffer(new_memory)
